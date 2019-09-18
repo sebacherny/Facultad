38f235fd5c31c5867b9de501714df69f0332d62e
cherny <cherny@brightsector.com>
2019-09-18 01:06:04 -0300

    Adding ej6; adding main function to use same ecm_a and ecm_b in both ex. 2/3 and the graph in 5; adding image with seed 1

--- Licenciatura/Estadistica/2019/TP2/tp2.R
+++ Licenciatura/Estadistica/2019/TP2/tp2.R
@@ -1,6 +1,13 @@
 # Link overleaf: https://www.overleaf.com/project/5d71179c4e863e000133fe9f
 # Link consignas: http://cms.dm.uba.ar/academico/materias/2docuat2019/estadistica_M/2019/tp2.pdf
 
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) > 0) {
+	print(paste("Using seed", args[1]));
+	set.seed(args[1]);
+}
+
+
 procedure_one_person <- function(p) {
 	dice <- sample(1:6, 1)
 	if (dice <= 2) {
@@ -10,16 +17,15 @@ procedure_one_person <- function(p) {
 }
 
 p_emv <- function(n, p, q=0) {
-	reality <- 0
+    reality <- 0
     for (person in 1:n) {
         reality <- reality + (runif(1) <= p) * (runif(1) >= q)
     }
-#    print(paste(reality, n))
     return (reality / n)
 }
 
 p_a <- function(n, p) {
-	reality <- 0
+    reality <- 0
     for (person in 1:n) {
         reality <- reality + procedure_one_person(p)
     }
@@ -29,31 +35,40 @@ p_a <- function(n, p) {
 p_b <- function(n, p) {
     tmp <- p_a(n, p)
     if (0 <= tmp && tmp <= 1) {
-		return (tmp);
-	}
-	if (tmp > 1) {
-		return (1)
-	}
-	return (0)
+        return (tmp);
+    }
+    if (tmp > 1) {
+        return (1)
+    }
+    return (0)
 #    return (tmp * (0 <= tmp && tmp <= 1) + (tmp > 1))
 }
+
+p_c_ej6 <- function(n, p) {
+    tmp <- p_a(n, p)
+    return (tmp/3 + 1/3);
+}
+
 
 monte_carlo <- function(func, n, p, n_rep=10) {
 	result <- 0
 	for (essay in 1:n_rep) {
-		
 		result <- result + ((func(n, p) - p) ^ 2)
 	}
 	return (result / n_rep)
 }
 
 ej2 <- function(n=1000, p=0.2) {
-	print(paste("El ECM de P_a es: ", monte_carlo(p_a, n, p)))
+	ecm_a = monte_carlo(p_a, n, p);
+	print(paste("El ECM de P_a es: ", ecm_a))
 	print(paste("El ECM del P_emv es: ", monte_carlo(p_emv, n, p)))
+	return (ecm_a)
 }
 
 ej3 <- function(n=1000, p=0.2) {
-	print(paste("El ECM de P_b es: ", monte_carlo(p_b, n, p)))
+	ecm_b = monte_carlo(p_b, n, p);
+	print(paste("El ECM de P_b es: ", ecm_b))
+	return (ecm_b);
 }
 
 ej4 <- function(n=1000, p=0.2, q=0.4) {
@@ -62,7 +77,7 @@ ej4 <- function(n=1000, p=0.2, q=0.4) {
 	return (e_q)
 }
 
-ej5 <- function(n_sample=100, n=1000, p=0.2) {
+ej5 <- function(ecm_a, ecm_b, n_sample=100, n=1000, p=0.2) {
 	q_vec <- c()
 	e_q_vec <- c()
 	for (i in 0:n_sample) {
@@ -70,22 +85,29 @@ ej5 <- function(n_sample=100, n=1000, p=0.2) {
 		q_vec <- c(q_vec, q)
 		e_q_vec <- c(e_q_vec, ej4(n, p, q))
 	}
-
-	a <- monte_carlo(p_a, n, p)
-	b <- monte_carlo(p_b, n, p)
-	print(paste(a, b));
+	# print(paste(a, b));
 	FILES_FOLDER = getwd();
 	filepath = paste(FILES_FOLDER, "ej5.png", sep="/");
 	png(filepath);
 	plot(q_vec, e_q_vec, xlab="q", ylab="e_q", pch=19);
-	abline(h=a);
-	abline(h=b);
+	abline(h=ecm_a);
+	abline(h=ecm_b);
 	print(paste("See file ", filepath));
 	dev.off();
 }
 
-ej2();
-ej3();
-ej4();
-ej5();
+ej6 <- function(n=1000, p=0.2) {
+        print(paste("El ECM de P_c (linealizar rango de p_a a [0,1])  es: ", monte_carlo(p_c_ej6, n, p)))
+}
+
+
+main <- function(){
+    ecm_a <- ej2();
+    ecm_b <- ej3();
+    print(paste("El ECM cuando q=0.4 es:", ej4()));
+    ej5(ecm_a, ecm_b);
+    ej6();
+}
+
+main();
 
