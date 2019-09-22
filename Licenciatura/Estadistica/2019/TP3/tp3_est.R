daa03df466992a171cf7da0bc192f9024d6e76f7
cherny <cherny@brightsector.com>
2019-09-22 16:44:30 -0300

    Adding TP3

---
+++ Licenciatura/Estadistica/2019/TP3/tp3_est.R
@@ -0,0 +1,49 @@
+## Resolucion del TP 3: Bootstrap
+
+#Fijamos el seed asi el programa es replicable
+set.seed(1234)
+
+## Funcion que dada una muestra calcula los intervalos IC_i(sigma2) para i = 1,2,3,4.
+calcularIntervalos = function(muestra, q_perc, alpha = 0.05, nboot = 2000){
+  estimador_muestra = quantile(muestra, q_perc)
+  quantiles_boot = rep(NA,nboot)
+  n = length(muestra);
+  for(j in 1:nboot){
+    muestra.boot = sample(muestra, size = n, replace = T)
+    quantiles_boot[j] = quantile(muestra.boot, q_perc)
+  }
+  var_quantiles_boot = var(quantiles_boot)
+  metodo_1 = c(estimador_muestra - sqrt(var_quantiles_boot) * qnorm(1-alpha/2) , estimador_muestra + sqrt(var_quantiles_boot) * qnorm(1-alpha/2))
+  metodo_2 = c(quantile(quantiles_boot, alpha/2), quantile(quantiles_boot, 1-alpha/2))
+  return(list(metodo1=metodo_1, metodo2=metodo_2))
+}
+
+show_results = function(muestra){
+	for(q in c(0.25, 0.5, 0.75)){
+		l = calcularIntervalos(muestra, q)
+		print(paste("Para el percentil ", q, "obtuve los siguientes intervalos de nivel 0.05: Metodo 1 = (", l$metodo1[1], ", ", l$metodo1[2], ") ; Metodo 2 = (", l$metodo2[1], ", ", l$metodo2[2], ")"));
+		print(paste("Cuando el percentil real de la muestra es: ", quantile(muestra, q)));
+		print("--------------------------------------------------");
+	}
+}
+
+read_file_and_show_results = function(filepath){
+	print(filepath);
+	if (length(filepath) == 0){
+		filepath = paste(FILES_FOLDER, "datos_titanic.csv", sep="/")
+	}
+	MyData <- read.csv(file=filepath, header=TRUE, sep=",")
+	show_results(MyData$Fare);
+}
+
+main = function(){
+	args = commandArgs(trailingOnly=TRUE)
+	filepath = "";
+	if (length(args) > 0) {
+		filepath = args[1];
+	}
+	read_file_and_show_results(filepath);
+}
+
+main();
+
